07a0bc26703efdf3123d8d44304c6e73afd07937
sureshjangid <sureshjangir902@gmail.com>
2024-04-14 19:33:38 +0530

    Design Patterns: Layout Components => Add SplitPage component implementation with LeftSide and RightSide components

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { SplitPage } from './components/split-page';
+
+
+const LeftSide = ({title})=>{
+  return (
+    <h1 style={{background:"red"}}> {title}</h1>
+  )
+}
+
+const RightSide = ({title})=>{
+  return (
+    <h1 style={{background:"pink"}}> {title}</h1>
+  )
+}
+function App() {
+  return (
+    <>
+    {/* We have way to used SplitPage  */}
+    {/* first one */}
+    {/* <SplitPage Left={LeftSide} Right={RightSide} leftWidth={1} rightWidth={3}/> */}
+
+    {/* second one */}
+    <SplitPage leftWidth={1} rightWidth={3}>
+    <LeftSide title={'I m LeftOne!'}/>
+    <RightSide title={'I m RightOne!'}/>
+    </SplitPage>
+
+    </>
+  );
+}
+
+export default App;
