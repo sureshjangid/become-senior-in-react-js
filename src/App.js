45fa0708d669fb5435c3a691cdbc4786b891f28b
sureshjangid <sureshjangir902@gmail.com>
2024-04-15 06:37:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import './App.css';
 import { SplitPage } from './components/split-page';
 
-
-const LeftSide = ({title})=>{
-  return (
-    <h1 style={{background:"red"}}> {title}</h1>
-  )
-}
-
-const RightSide = ({title})=>{
-  return (
-    <h1 style={{background:"pink"}}> {title}</h1>
-  )
-}
 function App() {
   return (
     <>
