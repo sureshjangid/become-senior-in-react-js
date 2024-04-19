725fb2eff7d4e0cc33698a0d29a2850d99860d60
sureshjangid <sureshjangir902@gmail.com>
2024-04-19 18:40:15 +0530

    hoc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import { logProps } from './components/log-props';
+import { UserInfo } from './components/user-info';
+
+
+const UserWrapperHOC =  logProps(UserInfo)
 function App() {
   return (
     <>
+    <UserWrapperHOC name="suresh" profile="Software Developer" company="Zentek"/>
     </>
   );
 }
