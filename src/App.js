18c82a20e5d4c71fdd389d41c427f60421638769
sureshjangid <sureshjangir902@gmail.com>
2024-04-21 11:40:47 +0530

    portal-for-alert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useState } from 'react';
 import './App.css';
+import { createPortal } from 'react-dom';
+import Alert from './components/alert';
+
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleAlert = () => {
+    setShow(true);
+  };
+
+  
+  const handleClose = () => {
+    setShow(false);
+  };
   return (
-    <>
-    </>
+    <div className="container">
+      <div className="other-components">
+        <h1>Other Components</h1>
+        <button onClick={handleAlert}>Alert</button>
+      <Alert  show={show} children onClose={handleClose}>
+      Successfully login
+      </Alert>
+      </div>
+    </div>
   );
 }
+
+
 
 export default App;
